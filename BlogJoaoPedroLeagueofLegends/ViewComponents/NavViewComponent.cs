db52b7dc8de6a58b647930944900378b1f6f12e4
Joao Pedro R Brito <jpedro0brito@gmail.com>
2019-06-18 20:06:51 -0400

    Add ViewComponent, Ajeitando Layout

---
+++ BlogJoaoPedroLeagueofLegends/ViewComponents/NavViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using BlogJoaoPedroLeagueofLegends.DoMain.Entities;
+using BlogJoaoPedroLeagueofLegends.DoMain.Services;
+using BlogJoaoPedroLeagueofLegends.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogJoaoPedroLeagueofLegends.ViewComponents
+{
+    public class NavViewComponent : ViewComponent
+    {
+        private readonly ICategoriaServices categoriaServices;
+        public NavViewComponent(ICategoriaServices categoriaServices)
+        {
+            this.categoriaServices = categoriaServices;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var mapCategoria = Mapper.Map<IEnumerable<Categoria>, IEnumerable<CategoriaViewModel>>(await categoriaServices.BuscaPorTudo());
+            return View(mapCategoria);
+        }
+    }
+}
